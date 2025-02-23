9d8a464e043e08a4d215894b63b46d71a5666c82
nyel-101 <dandevjr@gmail.com>
2025-02-23 20:17:06 +0000

    chore(worker): sanitize validation pipeline

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff1cb4619445489bbbafd761e1ad149a = () => {
-  return '2026-05-20T21:51:43Z';
+const node_ff12686c47934181bbc43d0a876fdace = () => {
+  return '2025-02-23T20:17:06Z';
 };
