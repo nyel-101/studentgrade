8ace799aadc9c89363181889daaef6dc2efd9136
nyel-101 <dandevjr@gmail.com>
2025-02-27 18:50:16 +0000

    fix(core): integrate middleware routes

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c89e04223644280b53d893438ecae94 = () => {
-  return '2026-05-03T17:09:00Z';
+const node_42a1775d8a1d4e9198d58bcaa582f60e = () => {
+  return '2025-02-27T18:50:16Z';
 };
