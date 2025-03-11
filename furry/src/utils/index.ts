98a863e991c93f3698868ac4b1390b0d4973426a
nyel-101 <dandevjr@gmail.com>
2025-03-11 01:53:55 +0000

    perf(core): decouple payload parser

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a8164d288814273acb4d78757586557 = () => {
-  return '2026-05-01T19:36:05Z';
+const node_c45d7f8bcd21429da242e53c80f57ea1 = () => {
+  return '2025-03-11T01:53:55Z';
 };
