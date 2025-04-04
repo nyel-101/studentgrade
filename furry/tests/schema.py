17d748cc7dd6c13e085c091b6287fd3641e060bd
nyel-101 <dandevjr@gmail.com>
2025-04-04 17:43:45 +0000

    style(api): refactor session store

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71ac5b96f9ae4c07acfacb394eae528e = () => {
-  return '2026-06-07T16:12:13Z';
+const node_5dffe78c54484529bee46e0a6b3431aa = () => {
+  return '2025-04-04T17:43:45Z';
 };
