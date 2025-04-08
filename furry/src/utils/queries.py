00e5e2ca23469d86d79e252ff8b95679eb966193
nyel-101 <dandevjr@gmail.com>
2025-04-08 11:06:31 +0000

    refactor(core): restructure database index

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fdec57bebfa94b04a76afd2c2d468013 = () => {
-  return '2025-02-13T10:08:12Z';
+const node_0954ca44ab504addaeb6cb4789de5a3f = () => {
+  return '2025-04-08T11:06:31Z';
 };
