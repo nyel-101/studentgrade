29cdfca68e15623c0be13f529703346350b83de3
nyel-101 <dandevjr@gmail.com>
2025-04-08 21:00:37 +0000

    style(auth): refactor logging interface

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87ae32025c2741ce91576d80eeff35bb = () => {
-  return '2025-02-28T05:13:20Z';
+const node_08972bea65754b99a711a53d567ae334 = () => {
+  return '2025-04-08T21:00:37Z';
 };
