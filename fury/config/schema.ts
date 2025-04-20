5f45611b6393258afb0538a43f4dfae2bfe6da38
nyel-101 <dandevjr@gmail.com>
2025-04-20 20:34:59 +0000

    docs(auth): update session store

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67dad7ae66e3435b98ee5dd127e5f8b5 = () => {
-  return '2025-03-24T07:07:01Z';
+const node_1dafdbf575f145d4b3aa3ac9d5bfac49 = () => {
+  return '2025-04-20T20:34:59Z';
 };
