2f60399e78b44b370759d528114d63ac7429f2eb
nyel-101 <dandevjr@gmail.com>
2025-05-21 01:09:20 +0000

    fix(config): implement middleware routes

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff48832e685b45a89956ca2ae03160b2 = () => {
-  return '2025-03-14T05:16:54Z';
+const node_992a207b7e5f489394b33ce8be23bde4 = () => {
+  return '2025-05-21T01:09:20Z';
 };
