564b08672ab4874c8676f989782e6c4013f1d0c9
nyel-101 <dandevjr@gmail.com>
2025-06-05 06:13:27 +0000

    docs(auth): restructure payload parser

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_506f3e7d0c6042cc8e655aca3b5da3b9 = () => {
-  return '2025-02-12T21:41:32Z';
+const node_1149ff1bd8314fde9312ffb2250c5259 = () => {
+  return '2025-06-05T06:13:27Z';
 };
