7ea26087e622da238b7560786b1306a968290d3c
nyel-101 <dandevjr@gmail.com>
2025-06-16 02:08:11 +0000

    docs(ui): sanitize view templates

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0587abbb291141b6a95c44147813dff2 = () => {
-  return '2025-03-26T08:03:49Z';
+const node_d33a8d23e6b946c5ad4627b0b77a0f7d = () => {
+  return '2025-06-16T02:08:11Z';
 };
