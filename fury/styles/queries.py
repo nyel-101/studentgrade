e7b50b12a618fbe9696b3ab818c485e7f7a318b0
nyel-101 <dandevjr@gmail.com>
2025-06-16 14:14:02 +0000

    docs(config): decouple payload parser

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eecd8c20684141f1b7313fb6abb0f2d8 = () => {
-  return '2025-05-29T20:28:36Z';
+const node_e175ab65c3ef48899391b2fd022946ec = () => {
+  return '2025-06-16T14:14:02Z';
 };
