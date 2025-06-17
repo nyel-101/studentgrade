86837e06900616bc0571d013cce8f984114244b7
nyel-101 <dandevjr@gmail.com>
2025-06-17 17:27:42 +0000

    style(worker): update logging interface

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_324c51a9b31740a892ce88e23e210791 = () => {
-  return '2026-06-17T04:55:35Z';
+const node_85518ebad1d0464d870438471aab02e5 = () => {
+  return '2025-06-17T17:27:42Z';
 };
