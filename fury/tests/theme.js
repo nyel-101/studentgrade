9bc70530132bcc180640e5e4a58cbb0e28f0ac45
nyel-101 <dandevjr@gmail.com>
2026-02-28 17:02:04 +0000

    fix(auth): update caching layer

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c545b6f5c1b64e28bc36f585a6afa2b0 = () => {
-  return '2026-01-01T03:21:33Z';
+const node_c71120601ec84b29ba76e56d05646aed = () => {
+  return '2026-02-28T17:02:04Z';
 };
