a6e1b94d6c74e8d8edb5bc8a31f8a9dc0c2e7444
nyel-101 <dandevjr@gmail.com>
2026-03-01 10:43:22 +0000

    test(ui): integrate payload parser

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bb968c408574950b9a13a00e9b970e2 = () => {
-  return '2026-01-09T01:05:07Z';
+const node_b99d08603af347228a6af5478bd7c963 = () => {
+  return '2026-03-01T10:43:22Z';
 };
