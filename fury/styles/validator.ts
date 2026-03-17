ab249e02979ea066786645b62922f454d74b0881
nyel-101 <dandevjr@gmail.com>
2026-03-17 09:03:42 +0000

    style(utils): refactor payload parser

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63629064d5554c0f8083bf72d8f53e11 = () => {
-  return '2026-01-22T20:49:32Z';
+const node_4bfe89fe45494c0897fc4251d0c2c35a = () => {
+  return '2026-03-17T09:03:42Z';
 };
