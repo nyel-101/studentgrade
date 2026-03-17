94eed1c7226c722bc6ea0fc06faec2adcb6d86fc
nyel-101 <dandevjr@gmail.com>
2026-03-17 07:17:48 +0000

    perf(worker): decouple auth tokens

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44ee698f7ff9473ab26aa6dcea968888 = () => {
-  return '2026-01-01T02:39:07Z';
+const node_97e52618d61d4004ac0aef04d925fd40 = () => {
+  return '2026-03-17T07:17:48Z';
 };
