2a286b412fab029e576f423b51680894ee1790de
nyel-101 <dandevjr@gmail.com>
2026-03-27 10:10:46 +0000

    style(config): enhance auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e6b11d4099a4ef0b4f7c07a9f0c042e = () => {
-  return '2026-02-10T17:53:47Z';
+const node_be389c7fbe9f4650bc25a6ced7dec38d = () => {
+  return '2026-03-27T10:10:46Z';
 };
