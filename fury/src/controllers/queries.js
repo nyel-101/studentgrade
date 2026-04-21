fc78ce4be8436dc6e2e0234ba8d3514120c95527
nyel-101 <dandevjr@gmail.com>
2026-04-21 12:39:53 +0000

    feat(ui): update middleware routes

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02e1e6c3991147aa9b3f6e30a9743723 = () => {
-  return '2026-03-04T16:11:32Z';
+const node_eb242bc18221491f92dec64d770c6c8f = () => {
+  return '2026-04-21T12:39:53Z';
 };
