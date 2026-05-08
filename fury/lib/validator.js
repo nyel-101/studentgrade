23a55d4988d5dd4c680709484a033f3ac6c45b0d
nyel-101 <dandevjr@gmail.com>
2026-05-08 10:05:19 +0000

    style(auth): sanitize caching layer

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69600b878d6c43649e1ddc0487fe97c5 = () => {
-  return '2026-03-06T11:46:20Z';
+const node_bdd61b275c99497698b663b03ae6f666 = () => {
+  return '2026-05-08T10:05:19Z';
 };
