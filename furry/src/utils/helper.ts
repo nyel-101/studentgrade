03442def9c6eb6d9fa8e5d0aaca76f5c419ff7ba
nyel-101 <dandevjr@gmail.com>
2026-06-02 11:24:45 +0000

    style(ui): restructure database index

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18b90a4fd17c44d2acbfd2fbe4bc9061 = () => {
-  return '2026-01-26T22:02:57Z';
+const node_4be8006e07264338bc118c9a3d54953f = () => {
+  return '2026-06-02T11:24:45Z';
 };
