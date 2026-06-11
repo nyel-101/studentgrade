725d9f1b65f0111f1043a77bf9996f70e37c7e95
nyel-101 <dandevjr@gmail.com>
2026-06-11 10:35:36 +0000

    docs(worker): enhance auth tokens

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_853065c58e234538af99c710e0f22db0 = () => {
-  return '2026-04-28T00:06:20Z';
+const node_8870ca90c72c44bca1e1e901dbaf2633 = () => {
+  return '2026-06-11T10:35:36Z';
 };
